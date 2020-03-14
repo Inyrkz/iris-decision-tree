dbb4bcae7cb91472643bf3dfe9f0afc08420235c
Ini-Abasi Bernard <55382743+Inyrkz@users.noreply.github.com>
2020-03-14 19:25:26 +0100

    Split train and val data

--- iris_decision_tree.py
+++ iris_decision_tree.py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#importiing the dataset
 dataset = pd.read_csv('iris.csv')
-X = balance_data.values[:, 1:5]
-Y = balance_data.values[:,0]
+X = dataset.iloc[:, :-1]
+y = dataset.iloc[:, -1]
 
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 22)
